49dbaa21e73d393e10e4083ab8c23c034710b342
Sahil Trikha <trikha93@gmail.com>
2014-02-14 15:10:41 -0500

    created schema

---
+++ db/migrate/20140214195236_basic.rb
@@ -0,0 +1,35 @@
+class Basic < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :first_name
+      t.string :last_name
+      t.text :facebook_account
+      t.text :twitter_account
+      t.timestamps
+    end
+    create_table :messages do |t|
+      t.text :text_message
+      t.text :video_link
+      t.text :photo_link
+      t.datetime :send_date
+      t.references :users
+      t.timestamps
+    end
+    create_table :recipients do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.text :facebook_account
+      t.text :twitter_account
+      t.references :users
+      t.timestamps
+    end
+    create_table :message_recipient do |t|
+      t.references :recipients
+      t.references :messages
+      t.timestamps
+    end
+  end
+end
