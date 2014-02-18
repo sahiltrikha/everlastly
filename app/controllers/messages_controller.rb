841537d037e2a394396fc5345498bcc4829b8607
Sahil Trikha <trikha93@gmail.com>
2014-02-18 14:24:32 -0500

    view message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,17 +6,21 @@ class MessagesController < ApplicationController
         render :new
     end
     def create
-       @user = User.find_by(id: params[:user_id])
-       @message = Message.new(user_params)
-       @message.user = @user
-       if @message.save
-         redirect_to "/users/1"
-     else
-         render(:new)
-     end
- end
+     @user = User.find_by(id: params[:user_id])
+     @message = Message.new(user_params)
+     @message.user = @user
+     if @message.save
+        redirect_to "/users/#{@user.id}/messages/#{@message.id}"
+    else
+       render(:new)
+    end
+    end
 
- def user_params
-    params.require(:message).permit(:text_message, :video_link, :photo_link, :send_date)
-end
+    def user_params
+        params.require(:message).permit(:text_message, :video_link, :photo_link, :send_date)
+    end
+    def show
+        @message = Message.find_by(id: params[:id])
+        render :show
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,11 +3,11 @@ class Message < ActiveRecord::Base
   has_many :message_recipients
 
   # validates :send_date, :user_id presence: true
-  # validates :has_message?
+   validate :has_message?
 
-  # def has_message?
-  #   if :text_message.blank? && :video_link.blank? & :photo_link.blank?
-  #    errors.add(:text_message, "Entire message can't be blank")
-  #   end
-  # end
+  def has_message?
+    if :text_message.blank? && :video_link.blank? & :photo_link.blank?
+     errors.add(:text_message, "Entire message can't be blank")
+    end
+  end
 end
