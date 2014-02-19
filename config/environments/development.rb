572d56ab20864372d99e804bf7f91c88e4282778
Sahil Trikha <trikha93@gmail.com>
2014-02-19 10:13:45 -0500

    added config for gmail with actionmailer -- no pass

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,19 @@ Everlastly::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # ST: SENDING MAIL, NEED TO KNOW IF IT FAILS!
+  config.action_mailer.raise_delivery_errors = true
+
+  #ST: from http://guides.rubyonrails.org/action_mailer_basics.html#action-mailer-configuration-for-gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            '',
+    password:             '',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
